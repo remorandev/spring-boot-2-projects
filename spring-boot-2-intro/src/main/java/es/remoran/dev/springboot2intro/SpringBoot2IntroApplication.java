3c72ec45d1ea631a42256fb30299a03630e9b255
Roberto Moran Recinos <remorandev@gmail.com>
2021-07-01 12:45:17 +0200

    -INIT proyecto base spring-boot-2-intro

---
+++ spring-boot-2-intro/src/main/java/es/remoran/dev/springboot2intro/SpringBoot2IntroApplication.java
@@ -0,0 +1,13 @@
+package es.remoran.dev.springboot2intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2IntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2IntroApplication.class, args);
+    }
+
+}
---
+++ spring-boot-2-intro/src/test/java/es/remoran/dev/springboot2intro/SpringBoot2IntroApplicationTests.java
@@ -0,0 +1,13 @@
+package es.remoran.dev.springboot2intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2IntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
