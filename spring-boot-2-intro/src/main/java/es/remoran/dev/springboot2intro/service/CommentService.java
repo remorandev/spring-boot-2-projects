f0f8a856c4b10b7cfdf79a51f53ed7918a468ed6
Roberto Moran Recinos <remorandev@gmail.com>
2021-07-30 08:30:17 +0200

    - Add: dependencia sprin-boot-starter-security - Add: servicios CommentService y UserService

---
+++ spring-boot-2-intro/src/main/java/es/remoran/dev/springboot2intro/service/CommentService.java
@@ -0,0 +1,34 @@
+package es.remoran.dev.springboot2intro.service;
+
+import es.remoran.dev.springboot2intro.model.Comment;
+import es.remoran.dev.springboot2intro.repo.CommentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class CommentService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommentService.class);
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public List<Comment> saveAll(List<Comment> comments) {
+        LOGGER.info("Saving {}", comments);
+        return commentRepository.saveAll(comments);
+    }
+
+    public List<Comment> getAllCommentFromToday() {
+        LocalDate now = LocalDate.now();
+        return commentRepository.findByCreatedYearAndMonthAndDay(now.getYear(), now.getMonth().getValue(), now.getDayOfMonth());
+    }
+}
