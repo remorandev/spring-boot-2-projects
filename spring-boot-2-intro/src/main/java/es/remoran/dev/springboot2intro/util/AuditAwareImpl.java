aed0268db8ee0d2b6b947865aed1be5b381e7f80
Roberto Moran Recinos <remorandev@gmail.com>
2021-10-29 17:08:13 +0200

    Configuración para habilitar auditoria de usuario.

---
+++ spring-boot-2-intro/src/main/java/es/remoran/dev/springboot2intro/util/AuditAwareImpl.java
@@ -0,0 +1,27 @@
+package es.remoran.dev.springboot2intro.util;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /*
+    * Configuración que permite habilitar agregar el usuario de la sesión en anotaciones como @CreatedBy
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(((User) authentication.getPrincipal()).getUsername());
+    }
+}
