c866c997e19ea0b36da3d54bb00a37941d2c449e
Roberto Moran Recinos <remorandev@gmail.com>
2021-11-10 10:58:27 +0100

    Modelo de base de datos elasticsearch  User y Article.

---
+++ spring-boot-2-blog/src/main/java/es/remoran/dev/springboot2blog/model/User.java
@@ -0,0 +1,25 @@
+package es.remoran.dev.springboot2blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "springboot2blog_user", type = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    private String description;
+}
